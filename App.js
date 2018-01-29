1c03cc3b95ec9da6f3755d5a3f2454bab5e6a130
nahid <getnahid@gmail.com>
2018-01-29 17:46:01 +0600

    Save user object in app.js state

--- App.js
+++ App.js
@@ -6,37 +6,90 @@
 
 import React, {Component} from 'react';
 import {StyleSheet, View} from 'react-native';
-import EventItemList from './components/event.item.list'
-import GoogleLogin from './components/GoogleLogin'
-import calendarEventsListService from './serivces/calendar.events.list.service'
+import EventItemList from './components/event.item.list';
+import GoogleLogin from './components/GoogleLogin';
+import calendarEventsListService from './serivces/calendar.events.list.service';
 import {GoogleSignin, GoogleSigninButton} from 'react-native-google-signin';
-
+import {Text, TouchableOpacity} from 'react-native'
 
 export default class App extends Component {
+  constructor (props) {
+    super (props);
+    this.onLoginSuccess = this.onLoginSuccess.bind (this);
+    this.onLoginFail = this.onLoginFail.bind (this);
+
+    this.state = {
+      user: null,
+    };
+  }
 
   state = {
     calendarEvents: {},
-    calendarEventsItems: []
-  }
+    calendarEventsItems: [],
+  };
 
   componentDidMount = () => {
-    this.setState({
-      calendarEvents: calendarEventsListService(),
-      calendarEventsItems: calendarEventsListService().items,
-    })
+    this.setState ({
+      calendarEvents: calendarEventsListService (),
+      calendarEventsItems: calendarEventsListService ().items,
+    });
+  };
+
+  render () {
+    // <View style={styles.container}>
+    //   <EventItemList calendarEventsItems={this.state.calendarEventsItems}/>
+    // </View>
+
+    if (!this.state.user) {
+      return (
+        <GoogleLogin
+          onLoginSuccess={this.onLoginSuccess}
+          onLoginFail={this.onLoginFail}
+        />
+      );
+    }
+
+    if (this.state.user) {
+      return (
+        <View style={styles.container}>
+          <Text style={{fontSize: 18, fontWeight: 'bold', marginBottom: 20}}>
+            Welcome {this.state.user.name}
+          </Text>
+          <Text>Your email is: {this.state.user.email}</Text>
+
+          <TouchableOpacity
+            onPress={() => {
+              this.signOut ();
+            }}
+          >
+            <View style={{marginTop: 50}}>
+              <Text>Log out</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      );
+    }
   }
 
-  render() {
-    return (
-        // <View style={styles.container}>
-        //   <EventItemList calendarEventsItems={this.state.calendarEventsItems}/>
-        // </View>
-        <GoogleLogin/>
-    );
+  onLoginSuccess (user) {
+    this.setState ({user: user});
+    console.log (user);
+  }
+
+  onLoginFail (error) {
+    console.log ('Google login error ' + error);
+  }
+
+  signOut() {
+    GoogleSignin.revokeAccess().then(() => GoogleSignin.signOut()).then(() => {
+      this.setState({user: null});
+      console.log('Google sign out');
+    })
+    .done();
   }
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create ({
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -47,5 +100,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  }
+  },
 });
--- components/GoogleLogin.js
+++ components/GoogleLogin.js
@@ -1,88 +1,57 @@
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-} from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, View} from 'react-native';
 
 import {GoogleSignin, GoogleSigninButton} from 'react-native-google-signin';
 
 export default class GoogleLogin extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: null
-    };
+  componentDidMount () {
+    this.setupGoogleSignin ();
   }
 
-  componentDidMount() {
-    this.setupGoogleSignin();
+  render () {
+    return (
+      <View style={styles.container}>
+        <GoogleSigninButton
+          style={{width: 120, height: 44}}
+          color={GoogleSigninButton.Color.Light}
+          size={GoogleSigninButton.Size.Icon}
+          onPress={() => {
+            this.signIn ();
+          }}
+        />
+      </View>
+    );
   }
 
-  render() {
-    if (!this.state.user) {
-      return (
-        <View style={styles.container}>
-          <GoogleSigninButton style={{width: 120, height: 44}} color={GoogleSigninButton.Color.Light} size={GoogleSigninButton.Size.Icon} onPress={() => { this.signIn(); }}/>
-        </View>
-      );
-    }
-
-    if (this.state.user) {
-      return (
-        <View style={styles.container}>
-          <Text style={{fontSize: 18, fontWeight: 'bold', marginBottom: 20}}>Welcome {this.state.user.name}</Text>
-          <Text>Your email is: {this.state.user.email}</Text>
-
-          <TouchableOpacity onPress={() => {this.signOut(); }}>
-            <View style={{marginTop: 50}}>
-              <Text>Log out</Text>
-            </View>
-          </TouchableOpacity>
-        </View>
-      );
-    }
-  }
-
-  async setupGoogleSignin() {
+  async setupGoogleSignin () {
     try {
-      await GoogleSignin.hasPlayServices({ autoResolve: true });
-      await GoogleSignin.configure({
+      await GoogleSignin.hasPlayServices ({autoResolve: true});
+      await GoogleSignin.configure ({
         webClientId: '788455155343-160l5akaku5j090stsa4634ecd4r6elj.apps.googleusercontent.com',
-        offlineAccess: false
+        offlineAccess: false,
       });
 
-      const user = await GoogleSignin.currentUserAsync();
-      console.log(user);
-      this.setState({user});
-    }
-    catch(err) {
-      console.log("Play services error", err.code, err.message);
+      const user = await GoogleSignin.currentUserAsync ();
+      console.log (user);
+      this.setState ({user});
+    } catch (err) {
+      console.log ('Play services error', err.code, err.message);
     }
   }
 
-signIn() {
-    GoogleSignin.signIn()
-    .then((user) => {
-      console.log(user);
-      this.setState({user: user});
-    })
-    .catch((err) => {
-      console.log('WRONG SIGNIN', err);
-    })
-    .done();
-  }
-
-signOut() {
-    GoogleSignin.revokeAccess().then(() => GoogleSignin.signOut()).then(() => {
-      this.setState({user: null});
-    })
-    .done();
+  signIn () {
+    GoogleSignin.signIn ()
+      .then (user => {
+        this.props.onLoginSuccess (user);
+      })
+      .catch (err => {
+        this.props.onLoginFail (err);
+      })
+      .done ();
   }
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create ({
   container: {
     flex: 1,
     justifyContent: 'center',
